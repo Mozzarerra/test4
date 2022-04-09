34b72ce1f60ca5cfdf4f9e8b1fc6ba67d15b7e3b
Pizza Mozzarella <163glonger@gmail.com>
2022-04-09 04:07:05 +0400

    done

--- AdditionTable.cs
+++ AdditionTable.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListExample
+{
+    class AdditionTable
+    {
+        static void Main()
+        {
+            List<string> additionTableList = new List<string>();
+            for (int a = 1; a <= 9; a++)
+            {
+                for (int b = 1; b <= 9; b++)
+                {
+                    additionTableList.Add($"{a} + {b} = {a + b}");
+                }
+            }
+            foreach (string line in additionTableList)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
